9ff2e1a4659cfc636ebc810bd3231bb4c7d3c0c1
unknown <leonyangelaa@gmail.com>
2022-05-06 15:36:13 +0700

    Fix vulnerabilities

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
-app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(3000, () => {
     console.log('App is running on port 3000');
